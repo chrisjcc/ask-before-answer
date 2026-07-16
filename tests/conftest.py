dceb94e1021a789a3e3a631edd8887064b7d5fea
Christian Contreras-Campana <chrisjcc.physics@gmail.com>
2026-07-15 20:23:38 -0400

    fix(format): fix E501 Line too long (98 > 88)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,18 @@
 import sys
 
-# Workaround for persistent PEFT / Transformers BloomPreTrainedModel import bug in CI environments
+# Workaround for persistent PEFT / Transformers BloomPreTrainedModel
+# import bug in CI environments
 try:
     import transformers
     if not hasattr(transformers, "BloomPreTrainedModel"):
         class DummyBloomPreTrainedModel:
             pass
-        transformers.BloomPreTrainedModel = DummyBloomPreTrainedModel
-        sys.modules['transformers'].BloomPreTrainedModel = DummyBloomPreTrainedModel
+
+        transformers.BloomPreTrainedModel = (
+            DummyBloomPreTrainedModel
+        )
+        sys.modules["transformers"].BloomPreTrainedModel = (
+            DummyBloomPreTrainedModel
+        )
 except ImportError:
     pass
