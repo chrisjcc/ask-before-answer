af125c982dd217acd370f0f32348daeb2ce75afe
Christian Contreras-Campana <chrisjcc.physics@gmail.com>
2026-07-15 20:15:31 -0400

    test: mock BloomPreTrainedModel in conftest to definitively bypass peft import crashes in CI

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import sys
+
+# Workaround for persistent PEFT / Transformers BloomPreTrainedModel import bug in CI environments
+try:
+    import transformers
+    if not hasattr(transformers, "BloomPreTrainedModel"):
+        class DummyBloomPreTrainedModel:
+            pass
+        transformers.BloomPreTrainedModel = DummyBloomPreTrainedModel
+        sys.modules['transformers'].BloomPreTrainedModel = DummyBloomPreTrainedModel
+except ImportError:
+    pass
